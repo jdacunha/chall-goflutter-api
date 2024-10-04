0420e82a5cd5de48a89081658247d1abff87c8cd
Jason Da Cunha <dacunha.jason@gmail.com>
2024-10-04 23:43:19 +0200

    feat: add db entities

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,5 +1,9 @@
 -- Drop tables
 DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "stands";
+DROP TABLE IF EXISTS "kermesses";
 
--- Drop custom types
-DROP TYPE IF EXISTS users_role_enum;
+-- Drop types
+DROP TYPE IF EXISTS user_role_enum;
+DROP TYPE IF EXISTS stand_type_enum;
+DROP TYPE IF EXISTS statut_enum;
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,5 +1,8 @@
--- Type: user_role_enum
+-- Enum Types
 CREATE TYPE user_role_enum AS ENUM ('ORGANISATEUR', 'TENEUR_STAND', 'PARENT', 'ENFANT');
+CREATE TYPE stand_type_enum AS ENUM ('VENTE', 'ACTIVITE');
+CREATE TYPE statut_enum AS ENUM ('STARTED', 'ENDED');
+
 
 -- Table: users
 CREATE TABLE "users" (
@@ -7,7 +10,27 @@ CREATE TABLE "users" (
   "name" VARCHAR(255) NOT NULL,
   "email" VARCHAR(255) UNIQUE NOT NULL,
   "password_hash" VARCHAR(255) NOT NULL,
-  "role" users_role_enum NOT NULL,
+  "role" user_role_enum NOT NULL,
   "jetons" INTEGER NOT NULL DEFAULT 0,
   "parent_id" INTEGER REFERENCES "users"("id") DEFAULT NULL
+);
+
+-- Table: stands
+CREATE TABLE "stands" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+  "name" VARCHAR(255) NOT NULL,
+  "description" TEXT DEFAULT '',
+  "type" stand_type_enum NOT NULL,
+  "price" INTEGER NOT NULL DEFAULT 0,
+  "stock" INTEGER NOT NULL DEFAULT 0
+);
+
+-- Table: kermesses
+CREATE TABLE "kermesses" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
+  "name" VARCHAR(255) NOT NULL,
+  "description" TEXT DEFAULT '',
+  "statut" statut_enum NOT NULL DEFAULT 'STARTED'
 );
